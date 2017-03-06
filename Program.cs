6e1a225719abbec5d89e4b0199a73eb287934bbc
Pete Rossman <Pete Rossman>
2017-03-06 11:41:38 -0500

    Delete array conversion, program just prints myInt value

--- Program.cs
+++ Program.cs
@@ -10,18 +10,9 @@ namespace NumberFlip
     {
         static void Main(string[] args)
         {
-            int myNum = 321;
-
-            public static int[] myArr(int myNum)
-        {
-            var numbers = new Stack<int>();
-
-            for <; n > 0; n /= 10
-                numbers.Push(ValueType % 10);
-
-            return numbers.ToArray;
-        }
-        Console.WriteLine(numbers);
+            var myNum = 321;
+            Console.WriteLine(myNum);
+            Console.ReadLine();
         }
     }
 }
